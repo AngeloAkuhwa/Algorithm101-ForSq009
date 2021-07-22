d5f9fdd6b9012f8355d746ec9960fed69a26224f
AngeloAkuhwa <angeloakuhwa@gmail.com>
2021-07-22 01:08:33 +0100

    updated the just pushed implementation

--- OOP-PrinciplesForSQ008/Inheritance.cs
+++ OOP-PrinciplesForSQ008/Inheritance.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP_PrinciplesForSQ008
 {
@@ -21,28 +22,41 @@ namespace OOP_PrinciplesForSQ008
         //}
     }
 
-    public class PartTimeEmployee
+    public class PartTimeEmployee:Employee
     {
-        public string FirstName;
-        public string LastName;
-        public string Email;
         public float HourlySalary;
-
-        public string GetFullName()
+        public override string GetFullName()
         {
-            return FirstName + " " + LastName;
+            return FirstName + LastName + "PartTimeEmployee";
         }
     }
-    public class FullTimeEmployee
+    public class FullTimeEmployee:Employee
     {
-        public string FirstName;
-        public string LastName;
-        public string Email;
-        public float HourlySalary;
-
-        public string GetFullName()
+   
+        public float MonthlySalary;
+        public override string GetFullName()
         {
-            return FirstName + " " + LastName;
+            return FirstName + LastName + "FullTime Employee";
         }
+
+    }
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Emailb { get; set; }
+        public Address Address { get; set; }
+        public abstract string GetFullName();
+        
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        public string StreetName { get; set; }
+        public Employee Employee { get; set; }
+        public string City { get; set; }
+        public string Town  { get; set; }
+        public string State  { get; set; }
     }
 }
